927a1ce5d75b071117d2659333cf34e2d8704bff
joecoxstrong <joecoxstrong@gmail.com>
2022-03-03 12:48:49 -0500

    created dinosaur class with name, attack power, health, and attacks method (without logic at this time).

---
+++ dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.attack_power = int(attack_power)
+        self.health = 100
+
+    def attacks(self, robot):
+        pass
