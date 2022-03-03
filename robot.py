4087f6fe22558735a17ced2804acdc7673bd86fd
joecoxstrong <joecoxstrong@gmail.com>
2022-03-03 12:43:03 -0500

    created fleet class with robots list and create fleet method (currently without logic).

--- robot.py
+++ robot.py
@@ -1,11 +1,13 @@
 from unicodedata import name
 from xml.dom.domreg import well_known_implementations
 
+from Weapon import Weapon
+
 
 class Robot():
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.weapon = Weapon 
+        self.weapon = Weapon
     def attack(self, dinosaur):
         pass  
